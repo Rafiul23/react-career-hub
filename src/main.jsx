32ee5884e1fd60045ceacb1f91112faafbceb505
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-06 10:31:50 +0600

    added other components

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,38 @@ import {
 } from "react-router-dom";
 import Roots from './Componnents/Roots';
 import Home from './Componnents/Home';
+import Jobs from './Componnents/Jobs';
+import AppliedJobs from './Componnents/AppliedJobs';
+import Blogs from './Componnents/Blogs';
+import Statistics from './Componnents/Statistics';
+import ErrorPage from './Componnents/ErrorPage';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Roots></Roots>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/jobs',
+        element: <Jobs></Jobs>
+      },
+      {
+        path: '/appliedjobs',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/blogs',
+        element: <Blogs></Blogs>
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>
       }
     ]
   },
