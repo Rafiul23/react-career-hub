818ffece4a245543758fa43de6ba607099bd21bb
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-06 12:11:03 +0600

    job category list part added

---
+++ src/Componnents/CategoryList.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const CategoryList = () => {
+  const [categories, SetCategores] = useState([]);
+
+  useEffect(() => {
+    fetch("data/categories.json")
+      .then((res) => res.json())
+      .then((data) => SetCategores(data));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <h1 className="text-5xl text-center">Job Category List</h1>
+      <p className="text-center py-3">
+        Explore thousands of job opportunities with all the information you
+        need. Its your future
+      </p>
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+      {
+        categories.map(category =><div className="p-4 bg-base-200 rounded-sm" key={category.id}>
+            <img src={category.logo} className="my-3" alt="logo" />
+            <h5 className="font-bold">{category.category_name}</h5>
+            <p>{category.availability}</p>
+        </div>)
+      }
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
--- src/Componnents/Home.jsx
+++ src/Componnents/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import CategoryList from "./CategoryList";
 
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>  
+          <CategoryList></CategoryList>
         </div>
     );
 };
