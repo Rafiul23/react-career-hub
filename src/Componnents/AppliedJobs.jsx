99087eed253b712c0909b156bbc22ee082af6add
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-06 21:21:14 +0600

    added filter button with options

--- src/Componnents/AppliedJobs.jsx
+++ src/Componnents/AppliedJobs.jsx
@@ -4,31 +4,29 @@ import { useEffect, useState } from "react";
 import { getSaveJobApplication } from "../utilities/localstorage";
 import AppliedJobsCard from "./AppliedJobsCard";
 
-
 const AppliedJobs = () => {
+  const jobs = useLoaderData();
 
-    const jobs = useLoaderData();
+  const [appliedJobs, setAppliedJobs] = useState([]);
 
-    const [appliedJobs, setAppliedJobs] = useState([]);
+  useEffect(() => {
+    const storedJobIds = getSaveJobApplication();
 
-    useEffect(()=>{
-        const storedJobIds = getSaveJobApplication();
+    // if(jobs.length > 0){
+    //   const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id) )
+    // }
 
-        // if(jobs.length > 0){
-        //   const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id) )
-        // } 
+    const jobsApplied = [];
+    for (const id of storedJobIds) {
+      const job = jobs.find((job) => job.id === id);
+      if (job) {
+        jobsApplied.push(job);
+      }
+    }
 
-        const jobsApplied = [];
-        for(const id of storedJobIds){
-            const job = jobs.find(job => job.id === id)
-            if(job){
-                jobsApplied.push(job);
-            }
-        }
-
-        setAppliedJobs(jobsApplied);
-        console.log(storedJobIds, jobs, jobsApplied);
-    }, [])
+    setAppliedJobs(jobsApplied);
+    console.log(storedJobIds, jobs, jobsApplied);
+  }, []);
 
   return (
     <div>
@@ -43,15 +41,26 @@ const AppliedJobs = () => {
           Applied Jobs: {appliedJobs.length}
         </h2>
       </div>
-        <div className="grid grid-cols-1 gap-4 my-10">
-        {
-            appliedJobs.map(job => <AppliedJobsCard
-            key={job.id}
-            job={job}
-            ></AppliedJobsCard>)
-        }
-        </div>
+      <div className="text-right my-10">
+        <details className="dropdown">
+          <summary className="px-5 border border-violet-600 py-2 font-extrabold text-violet-600 rounded-md m-1">Filter By</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li>
+              <a>On Site</a>
+            </li>
+            <li>
+              <a>Remote</a>
+            </li>
+          </ul>
+        </details>
+        
+      </div>
 
+      <div className="grid grid-cols-1 gap-4 my-10">
+        {appliedJobs.map((job) => (
+          <AppliedJobsCard key={job.id} job={job}></AppliedJobsCard>
+        ))}
+      </div>
     </div>
   );
 };
